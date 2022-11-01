0ee9d42734c8f8c3247bc87eb1b095e3db515b46
choibohui <123dfga@naver.com>
2022-11-01 16:03:26 +0900

    20221101

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
-import React from 'react';
+import React from 'react'
 import List from './List';
-import './common.scss';
+import './common.scss'
+import { Link, Route, Routes } from 'react-router-dom';
+import Main from './Main';
+import Header from './Header';
+import Glist from './Glist';
+import All from './All';
 
 const App = () => {
-    ;
+    const genreList = [
+        "Action",
+        "Adventure",
+        "Animation",
+        "Comedy",
+        "Crime",
+        "Drama",
+        "Fantasy",
+        "Romance",
+        "Thriller",
+        "Western"
+    ]
     return (
         <div>
+
+            <Header>
+                <ul className='flex'>
+                    {
+                        genreList.map(it => {
+                            return (
+                                <li>
+                                    <Link to={it}>{it}</Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </Header>
+            <Routes>
+                <Route path="/" element={<Main limit={50} />} />
+                {
+                    genreList.map(it => {
+                        return (
+                            <Route path={it} element={<Glist genre={it} limit={20} />} />
+                        )
+                    })
+                }
+            </Routes>
+            <All />
+
             <List genre='Drama' limit={20} />
             <List genre='Action' limit={20} />
             <List genre='Horror' limit={20} />
         </div>
-    );
+    )
 }
-    ;
-export default App;
+
+export default App
--- src/List.js
+++ src/List.js
@@ -1,31 +1,31 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Load from './Load';
 
 const List = ({ genre, limit }) => {
-    // 데이터 가져오기
+    //데이터 가져오기
     const [movie, getMovie] = useState([]);
     const [load, setLoad] = useState(true);
-    console.log(genre, limit)
     const movieData = async () => {
-        const movie = await axios.get(`https://yts.mx/api/v2/list_movies.json?limit=${limit}&genre=${genre}`)
-        console.log(movie.data.data.movies);
+        setLoad(true)
+        const movie = await axios.get(`https://yts.mx/api/v2/list_movies.json?limit=${limit}&genre=${genre}`);
         getMovie(movie.data.data.movies);
-        setLoad(false);
+        setLoad(false)
     }
     useEffect(() => {
         movieData()
-    }, [])
+    }, [genre])
     return (
         <div>
             {
                 load
-                    ? <div>로딩중</div>
+                    ? <Load />
                     :
                     <ul className='List'>
                         {
                             movie.map(it => {
                                 return (
-                                    <li kdy={it.id}>
+                                    <li key={it.id}>
                                         <figure>
                                             <img src={it.medium_cover_image} alt={it.title} />
                                         </figure>
@@ -40,4 +40,4 @@ const List = ({ genre, limit }) => {
     )
 }
 
-export default List;
+export default List
