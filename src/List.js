19ee94b84361ff06f768e38a2c0ed4a58f17def1
choibohui <123dfga@naver.com>
2022-11-01 10:16:48 +0900

    20221101

--- src/List.js
+++ src/List.js
@@ -1,10 +1,11 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const List = () => {
     // 데이터 가져오기
     const [movie, getMovie] = useState([]);
     const movieData = async () => {
-
+        const movie = await axios.get('https://yts.mx/api/v2/list_movies.json')
     }
     useEffect(() => {
 
