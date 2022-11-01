9faae53ca37bdbb421dcd3e623e392832b7c11e8
choibohui <123dfga@naver.com>
2022-11-01 10:14:10 +0900

    20221101

---
+++ src/List.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+const List = () => {
+    // 데이터 가져오기
+    const [movie, getMovie] = useState([]);
+    const movieData = async () => {
+
+    }
+    useEffect(() => {
+
+    }, [])
+    return (
+        <div>List</div>
+    )
+}
+
+export default List;
