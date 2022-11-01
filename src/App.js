1d5d8e7973338ec8bdab880060fbd0889016ed7f
choibohui <123dfga@naver.com>
2022-11-01 11:20:33 +0900

    20221101

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-;
+import List from './List';
+import './common.scss';
+
 const App = () => {
     ;
     return (
-        <div>App</div>
+        <div>
+            <List genre='Drama' limit={20} />
+            <List genre='Action' limit={20} />
+            <List genre='Horror' limit={20} />
+        </div>
     );
 }
     ;
