9a72eae64c6550109203a794183d2daba5ee4194
choibohui <123dfga@naver.com>
2022-11-08 09:16:02 +0900

    20221107

--- src/Load.js
+++ src/Load.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Load = () => {
     return (
         <div className='Load'>
-            <i class="xi-spinner-1 xi-spin"></i>
+            <i className="xi-spinner-1 xi-spin"></i>
         </div>
     )
 }
 
-export default Load;
+export default Load
